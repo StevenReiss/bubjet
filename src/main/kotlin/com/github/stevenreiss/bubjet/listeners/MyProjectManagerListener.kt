0ae086f108d86ef088ad45bc9b90f24581c5d252
GitHub Action <action@github.com>
2021-12-29 14:56:14 +0000

    Template cleanup

--- src/main/kotlin/com/github/stevenreiss/bubjet/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/stevenreiss/bubjet/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.stevenreiss.bubjet.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.stevenreiss.bubjet.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/stevenreiss/bubjet/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/stevenreiss/bubjet/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.stevenreiss.bubjet.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.stevenreiss.bubjet.MyBundle
 
 class MyApplicationService {
 
